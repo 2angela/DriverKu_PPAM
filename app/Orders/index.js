a9054b45244120474d29e0085bd0313ddcb85095
angela <angelageraldine2@gmail.com>
2024-05-01 13:42:04 +0700

    chore: order history

--- app/Orders/index.js
+++ app/Orders/index.js
@@ -1,4 +1,4 @@
-import { Text, View } from "react-native";
+import { Text, View, StyleSheet, FlatList } from "react-native";
 import { Link, router } from "expo-router";
 import firestore from "@react-native-firebase/firestore";
 import { useAuth } from "../../auth/AuthProvider";
@@ -9,102 +9,118 @@ import NavigationBar from "../components/navigationbar";
 export default function Orders() {
   const { user } = useAuth();
   const [orders, setOrders] = useState([]);
-  const order_time = "test";
-  const status = "test";
 
   useEffect(() => {
-    const getOrders = async () => {
+    const fetchData = async () => {
       try {
-        const querySnapshot = firestore()
+        const querySnapshot = await firestore()
           .collection("Customer")
           .doc(user?.uid)
-          .collection("Order");
-        const response = await querySnapshot.get();
-        return response;
+          .collection("Order")
+          .orderBy("created_at")
+          .get();
+        const data = querySnapshot.docs.map((doc) => ({
+          id: doc.id,
+          driver: doc.data().driver == null ? "Unassigned" : doc.data().driver,
+          created_at: doc.data().created_at.toDate().toDateString(),
+          status: doc.data().status,
+        }));
+
+        setOrders(data);
+        console.log("Data:", data);
       } catch (error) {
-        return error;
+        console.error("Error fetching data:", error);
+        console.log("Data:", data);
       }
     };
-    getOrders().then((docs) => {
-      const data = docs.docs.map((doc) => doc.data());
-      setOrders(data);
-    });
+
+    fetchData();
   }, []);
 
-  return (
-    <View
-      style={{
-        flex: 1,
-        alignItems: "center",
-        justifyContent: "start",
-        paddingTop: 60,
-      }}
-    >
-      <Link href={"./order/details"}>order details</Link>
-      <View style={{ flexDirection: "row" }}>
-        <View style={{ width: "40%" }}>
-          <IconButton
-            onPress={() => {
-              router.push("/Home");
-            }}
-            icon="arrow-left"
-          />
-        </View>
-        <View style={{ width: "60%" }}>
-          <Text
-            style={{
-              fontSize: 30,
-              fontWeight: "bold",
-            }}
-          >
-            Orders
-          </Text>
-        </View>
-      </View>
-
+  const Item = ({ item }) => (
+    <View style={styles.card}>
       <View
         style={{
-          backgroundColor: "#F0F3FF",
-          borderColor: "black",
-          borderWidth: 1,
-          borderRadius: 15,
-          width: "80%",
+          flexDirection: "row",
+          justifyContent: "space-between",
         }}
       >
         <View
           style={{
-            flexDirection: "row",
             justifyContent: "space-between",
+            paddingHorizontal: 20,
+            paddingVertical: 10,
           }}
         >
-          <View
-            style={{
-              justifyContent: "space-between",
-              paddingHorizontal: 20,
-              paddingVertical: 10,
+          <Text style={styles.contents}>Driver: {item.driver}</Text>
+          <Text style={styles.contents}>{item.created_at}</Text>
+          <Text style={styles.contents}>{item.status}</Text>
+        </View>
+        <View style={{ paddingHorizontal: 10 }}>
+          <IconButton
+            onPress={() => {
+              router.push("");
             }}
-          >
-            <Text>Driver: </Text>
-            <Text>{order_time}</Text>
-            <Text>{status}</Text>
-          </View>
-          <View style={{ paddingHorizontal: 10 }}>
-            <IconButton
-              onPress={() => {
-                router.push("");
-              }}
-              icon="message-reply-text"
-            />
-            <IconButton
-              onPress={() => {
-                router.push("");
-              }}
-              icon="star-settings-outline"
-            />
-          </View>
+            icon="message-reply-text"
+            iconColor="#211951"
+            size={30}
+          />
+          <IconButton
+            onPress={() => {
+              router.push("");
+            }}
+            icon="star-settings-outline"
+            iconColor="#211951"
+            size={30}
+          />
         </View>
       </View>
+    </View>
+  );
+
+  return (
+    <View style={styles.container}>
+      <Link href={"./order/details"}>order details</Link>
+      <Text style={styles.title}>Orders</Text>
+      <FlatList
+        data={orders}
+        renderItem={Item}
+        keyExtractor={(item) => item.id}
+        contentContainerStyle={styles.list}
+      ></FlatList>
       <NavigationBar page="orders" />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "start",
+    paddingTop: 60,
+  },
+  list: {
+    flex: 3,
+    width: "100%",
+    paddingTop: 20,
+  },
+  card: {
+    backgroundColor: "#F0F3FF",
+    borderColor: "black",
+    borderWidth: 1,
+    borderRadius: 15,
+    marginVertical: 10,
+    width: 300,
+  },
+  title: {
+    fontSize: 32,
+    fontFamily: "MontserratExtraBold",
+    paddingVertical: 5,
+  },
+  contents: {
+    fontSize: 14,
+    fontFamily: "MontserratExtraBold",
+    paddingVertical: 5,
+  },
+});
