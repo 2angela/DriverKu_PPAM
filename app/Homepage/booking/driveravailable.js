ebcfc0fb16d6040ea771e0d5fc359861451f9162
angela <angelageraldine2@gmail.com>
2024-05-08 18:55:23 +0700

    style: prettier

--- app/Homepage/booking/driveravailable.js
+++ app/Homepage/booking/driveravailable.js
@@ -1,192 +1,212 @@
-import { View, StyleSheet, Text, Image, TouchableOpacity, ScrollView } from "react-native";
+import {
+  View,
+  StyleSheet,
+  Text,
+  Image,
+  TouchableOpacity,
+  ScrollView,
+} from "react-native";
 import NavigationBar from "../../components/navigationbar";
 import { useCallback, useState } from "react";
 import { useFocusEffect } from "expo-router";
 import firestore from "@react-native-firebase/firestore";
 
 export default function DriverAvailable({ navigation, route }) {
-    const { location, vehicle_types, dataBooking } = route.params
-    const [dataDriver, setDataDriver] = useState([])
+  const { location, vehicle_types, dataBooking } = route.params;
+  const [dataDriver, setDataDriver] = useState([]);
 
-    useFocusEffect(
-        useCallback(() => {
-            const fetchData = async () => {
-                try {
-                    const querySnapshot = await firestore()
-                    .collection("Driver")
-                    .where('availability', "==", true)
-                    .where('vehicle_types', "==", vehicle_types)
-                    .where('service_area', "array-contains", location)
-                    .get();
+  useFocusEffect(
+    useCallback(() => {
+      const fetchData = async () => {
+        try {
+          const querySnapshot = await firestore()
+            .collection("Driver")
+            .where("availability", "==", true)
+            .where("vehicle_types", "==", vehicle_types)
+            .where("service_area", "array-contains", location)
+            .get();
 
-                    const data = querySnapshot.docs.map((doc) => ({
-                        id: doc.id,
-                        rate: doc.data().rate,
-                        name: doc.data().name,
-                        avg_rating : doc.data().avg_rating,
-                        service_area : doc.data().service_area
-                    }));
-                    
-                    setDataDriver(data);
-                } catch (error) {
-                    console.error("Error fetching data:", error);
-                }
-            };
-        
-            fetchData();
-        }, [location])
-    )
+          const data = querySnapshot.docs.map((doc) => ({
+            id: doc.id,
+            rate: doc.data().rate,
+            name: doc.data().name,
+            avg_rating: doc.data().avg_rating,
+            service_area: doc.data().service_area,
+          }));
 
-    return (
-        <View style={styles.container}>
-            <View style={styles.textRow}>
-                {
-                    dataDriver.length > 0 &&
-                    <Text style={styles.title}>
-                        Drivers Avaliable
-                    </Text>
-                }
-            </View>
+          setDataDriver(data);
+        } catch (error) {
+          console.error("Error fetching data:", error);
+        }
+      };
 
-            <View style={styles.boxContainer}>
-                <ScrollView>
-                    <View style={styles.containerList}>
-                        {
-                            dataDriver.length > 0 ?
-                                dataDriver.map((e, i) => {
-                                    return (
-                                        <View key={i} style={styles.listStyle}>
-                                            <TouchableOpacity style={{ flex:1, flexDirection:"row" }} onPress={() => navigation.navigate("DriverDetail", {
-                                                idDriver : e.id,
-                                                dataBooking : dataBooking
-                                            })}>
-                                                <View style={{ width:60 }}>
-                                                    <Image source={require("../../../assets/driverdetails.png")} style={styles.imageContainer}/>
-                                                </View>
-                                                <View style={{ flex: 1 }}>
-                                                    <View>
-                                                        <Text style={styles.textName}>{e.name}</Text>
-                                                    </View>
-                                                    <View style={{ marginTop:2 }}>
-                                                        <Text style={styles.textRate}>Rate : Rp {e.rate.toLocaleString("id-ID")}/hour</Text>
-                                                    </View>
-                                                </View>
-                                                <View style={styles.containerRating}>
-                                                    <View>
-                                                        <Text style={styles.textRating}>{e.avg_rating}</Text>
-                                                    </View>
-                                                    <View style={{ marginHorizontal:5 }}>
-                                                        <Image source={require("../../../assets/icon_star.png")} style={{ width :15, height:15 }}/>
-                                                    </View>
-                                                </View>
-                                            </TouchableOpacity>
-                                        </View>
-                                    )
-                                })
-                            : 
-                                <View style={{ flex:1, justifyContent:"center", alignContent:"center" }}>
-                                    <View>
-                                        <View style={{ alignItems:"center" }}>
-                                            <Text style={styles.title}>
-                                                Driver not found !
-                                            </Text>
-                                        </View>
-                                    </View>
-                                    <View style={{ alignItems:"center", marginTop:3 }}>
-                                        <Text style={{ fontFamily: "MontserratSemiBold", }}>
-                                            Currently driver not avaliable in your area
-                                        </Text>
-                                    </View>
-                                </View>
-                        }
-                    </View>
-                </ScrollView>
-            </View>
-            <NavigationBar page="home" />
-        </View>
-    );
+      fetchData();
+    }, [location])
+  );
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.textRow}>
+        {dataDriver.length > 0 && (
+          <Text style={styles.title}>Drivers Avaliable</Text>
+        )}
+      </View>
+
+      <View style={styles.boxContainer}>
+        <ScrollView>
+          <View style={styles.containerList}>
+            {dataDriver.length > 0 ? (
+              dataDriver.map((e, i) => {
+                return (
+                  <View key={i} style={styles.listStyle}>
+                    <TouchableOpacity
+                      style={{ flex: 1, flexDirection: "row" }}
+                      onPress={() =>
+                        navigation.navigate("DriverDetail", {
+                          idDriver: e.id,
+                          dataBooking: dataBooking,
+                        })
+                      }
+                    >
+                      <View style={{ width: 60 }}>
+                        <Image
+                          source={require("../../../assets/driverdetails.png")}
+                          style={styles.imageContainer}
+                        />
+                      </View>
+                      <View style={{ flex: 1 }}>
+                        <View>
+                          <Text style={styles.textName}>{e.name}</Text>
+                        </View>
+                        <View style={{ marginTop: 2 }}>
+                          <Text style={styles.textRate}>
+                            Rate : Rp {e.rate.toLocaleString("id-ID")}/hour
+                          </Text>
+                        </View>
+                      </View>
+                      <View style={styles.containerRating}>
+                        <View>
+                          <Text style={styles.textRating}>{e.avg_rating}</Text>
+                        </View>
+                        <View style={{ marginHorizontal: 5 }}>
+                          <Image
+                            source={require("../../../assets/icon_star.png")}
+                            style={{ width: 15, height: 15 }}
+                          />
+                        </View>
+                      </View>
+                    </TouchableOpacity>
+                  </View>
+                );
+              })
+            ) : (
+              <View
+                style={{
+                  flex: 1,
+                  justifyContent: "center",
+                  alignContent: "center",
+                }}
+              >
+                <View>
+                  <View style={{ alignItems: "center" }}>
+                    <Text style={styles.title}>Driver not found !</Text>
+                  </View>
+                </View>
+                <View style={{ alignItems: "center", marginTop: 3 }}>
+                  <Text style={{ fontFamily: "MontserratSemiBold" }}>
+                    Currently driver not avaliable in your area
+                  </Text>
+                </View>
+              </View>
+            )}
+          </View>
+        </ScrollView>
+      </View>
+      <NavigationBar page="home" />
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        alignItems: "center",
-        justifyContent: "flex-start",
-        paddingTop: 50,
-        flexDirection: "col",
-    },
-    topImage: {
-        width: "60%",
-        height: "30%",
-        resizeMode: "cover",
-        marginBottom: 20,
-    },
-    textRow: {
-        width: "100%",
-        padding: 5,
-        alignItems: "center",
-    },
-    title: {
-        fontSize: 28,
-        fontFamily: "MontserratExtraBold",
-        color: "#836FFF",
-    },
-    text: {
-        marginTop: 25,
-        marginBottom: 8,
-        textAlign: "center",
-        fontFamily: "MontserratSemiBold",
-        fontSize: 18,
-        color: "#211951",
-    },
-    boxContainer: {
-        flexDirection: "row",
-        justifyContent: "space-around",
-        width: "100%",
-        paddingHorizontal: 20,
-        marginTop: 10,
-    },
-    containerList : { 
-        flexDirection:"column", 
-        flex:1, 
-        marginBottom:20
-    },
-    listStyle : { 
-        flex:1, 
-        flexDirection:"row", 
-        alignItems:"center", 
-        borderWidth:1, 
-        borderColor:"#bab8c3", 
-        borderRadius:10, 
-        padding:15, 
-        backgroundColor:"#f0f2fe", 
-        marginVertical:5, 
-        height:80 
-    },
-    imageContainer : { 
-        width:50, 
-        height:50 
-    },
-    textName : { 
-        color:"#6b6881", 
-        fontWeight:"700", 
-        fontSize:17, 
-        fontFamily: "MontserratExtraBold" 
-    },
-    textRate : { 
-        color:"#6b6881", 
-        fontFamily: "MontserratSemiBold", 
-        fontSize:12
-    },
-    containerRating : { 
-        width:60, 
-        alignItems:"center", 
-        flexDirection:"row", 
-        justifyContent:"flex-end"
-    },
-    textRating : { 
-        color:"#1b134c", 
-        fontWeight:"800", 
-        fontFamily: "MontserratExtraBold" 
-    }
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "flex-start",
+    paddingTop: 50,
+    flexDirection: "col",
+  },
+  topImage: {
+    width: "60%",
+    height: "30%",
+    resizeMode: "cover",
+    marginBottom: 20,
+  },
+  textRow: {
+    width: "100%",
+    padding: 5,
+    alignItems: "center",
+  },
+  title: {
+    fontSize: 28,
+    fontFamily: "MontserratExtraBold",
+    color: "#836FFF",
+  },
+  text: {
+    marginTop: 25,
+    marginBottom: 8,
+    textAlign: "center",
+    fontFamily: "MontserratSemiBold",
+    fontSize: 18,
+    color: "#211951",
+  },
+  boxContainer: {
+    flexDirection: "row",
+    justifyContent: "space-around",
+    width: "100%",
+    paddingHorizontal: 20,
+    marginTop: 10,
+  },
+  containerList: {
+    flexDirection: "column",
+    flex: 1,
+    marginBottom: 20,
+  },
+  listStyle: {
+    flex: 1,
+    flexDirection: "row",
+    alignItems: "center",
+    borderWidth: 1,
+    borderColor: "#bab8c3",
+    borderRadius: 10,
+    padding: 15,
+    backgroundColor: "#f0f2fe",
+    marginVertical: 5,
+    height: 80,
+  },
+  imageContainer: {
+    width: 50,
+    height: 50,
+  },
+  textName: {
+    color: "#6b6881",
+    fontWeight: "700",
+    fontSize: 17,
+    fontFamily: "MontserratExtraBold",
+  },
+  textRate: {
+    color: "#6b6881",
+    fontFamily: "MontserratSemiBold",
+    fontSize: 12,
+  },
+  containerRating: {
+    width: 60,
+    alignItems: "center",
+    flexDirection: "row",
+    justifyContent: "flex-end",
+  },
+  textRating: {
+    color: "#1b134c",
+    fontWeight: "800",
+    fontFamily: "MontserratExtraBold",
+  },
 });
